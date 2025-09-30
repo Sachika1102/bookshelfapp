1b740713a73caae4f604245ace1af1ad95bde5f5
Sachika <s.tsukeyanagi.sys23@morijyobi.ac.jp>
2025-09-30 18:47:10 +0900

    fix: add render domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ DATABASES = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
